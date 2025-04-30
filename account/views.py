ced281f84a252bc36f2f32be47c91535ea6e6779
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-04-30 21:50:23 +0200

    Implement user signup functionality with form handling and URL routing

--- account/views.py
+++ account/views.py
@@ -1,2 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignup
 
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserSignup(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            return redirect("index")
+    else:
+        form = UserSignup()
+    
+    return render(request, "account/signup.html", context={"form": form})
